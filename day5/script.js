0ee1f3faa51633e6452b214c89f9181e1a3890de
csdsyogesh <yadavy694@gmail.com>
2024-10-15 15:20:32 +0530

    heading

---
+++ day5/script.js
@@ -0,0 +1,22 @@
+// /*let x=confirm("are you sure logout");
+// alert(x);*/
+// // let v=prompt("enetr you name","rahul");
+// // alert(v);
+// // let disp=()=>{
+// //     console.log("welcome mere bhai")
+// // };
+// // disp();
+// let sum=(a,b)=>{
+//     return a+b;
+// }
+//     let sume= sum(2,5);
+//     console.log(sume);
+let p1=document.querySelectorAll('p');
+         let display=()=>{
+              p1[0].innerHTML="updated";
+              p1[0].style.backgroundColor='red';
+              p1[1].innerHTML="updated";
+              p1[1].style.backgroundColor='red';
+              p1[2].innerHTML="updated";
+              p1[2].style.backgroundColor='red';
+            }
